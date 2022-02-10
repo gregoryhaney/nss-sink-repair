51728a4a4e1889fcc5dc740fff0e5befd75a31b7
Gregory Haney <gregory.haney@gmail.com>
2022-02-10 16:02:48 -0600

    v1: Most functionality.

---
+++ src/scripts/Requests.js
@@ -0,0 +1,38 @@
+import { getRequests, deleteRequest } from "./dataAccess.js"
+
+
+
+const convertRequests = (request) => {
+    return `
+            <li>
+            ${request.description}
+            <button class="request__delete" id="request--${request.id}">Delete</button>
+            </li>
+    
+    `
+}
+
+
+
+export const Requests = () => {
+    const requests = getRequests()
+
+    let html = `
+        <ul>
+            ${
+                requests.map(convertRequests).join("")
+            }
+        </ul>
+    `
+
+    return html
+}
+
+const mainContainer = document.querySelector("#container")
+
+mainContainer.addEventListener("click", click => {
+        if (click.target.id.startsWith("request--")) {
+            const [,requestId] = click.target.id.split("--")
+            deleteRequest(parseInt(requestId))
+        }
+    })
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,23 @@
 import { SinkRepair } from "./SinkRepair.js"
+import { fetchRequests } from "./dataAccess.js"
 
 
 const mainContainer = document.querySelector("#container")
 
 const render = () => {
+    fetchRequests().then(
+        () => {
     mainContainer.innerHTML = SinkRepair()
-}
+        }
+    )
+}           
 
 render()
 
+
+mainContainer.addEventListener(
+    "stateChanged",
+    customEvent => {
+            render()
+    }
+)
